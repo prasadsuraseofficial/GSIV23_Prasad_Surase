7fb28c9ee0ffd2fe0dfed6d0668af98905fe82c1
Prasad Surase <prasad.surase@coriolis.co.in>
2023-08-20 01:57:16 +0530

    added custom dark/light theme - partially done

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import { theme, darkTheme } from "./theme/theme";
 import App from "./App.jsx";
 import "./index.css";
 // MUI font imports
@@ -7,12 +10,15 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { BrowserRouter } from "react-router-dom";
+
+const DARK_MODE_ON = false;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={DARK_MODE_ON ? darkTheme : theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/theme/theme.js
@@ -0,0 +1,39 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#F25C00", // Orange
+    },
+    background: {
+      default: "#FFFFFF", // White
+    },
+    text: {
+      primary: "#4A4A4A", // Gray
+    },
+    danger: {
+      main: "#CF3721", //red
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#F25C00", // Orange
+    },
+    background: {
+      default: "#4A4A4A", // Gray for dark mode background
+    },
+    text: {
+      primary: "#FFFFFF", // White text for dark mode
+    },
+    danger: {
+      main: "#F25C00", //orange
+    },
+  },
+});
+
+export { theme, darkTheme };
